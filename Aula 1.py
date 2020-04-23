f7f2936e2e5d458d328321a00865e83f95cc4f18
Edward Konig <edwardkonig@gmail.com>
2020-04-23 18:21:34 -0300

    creating virtual environment
    Co-authored-by: Marco Aurélio de Araújo Silva <marcoaasilva@gmail.com>

--- Aula 1.py
+++ Aula 1.py
@@ -1,26 +1,38 @@
 import csv
 
-lista_perguntas = [
-   "Qual é o seu nome?", 
-   "Qual é a sua idade?",
-   "Qual é o seu sexo?",
-   "Onde você mora?"
-  ]
+dict_perguntas = {
+  "nome":  "Qual é o seu nome?", 
+   "idade": "Qual é a sua idade?",
+   "sexo": "Qual é o seu sexo?",
+   "cidade": "Onde você mora?"
+}
 
+dict_respostas = {}
 lista_respostas = []
+while (True):
+  
+    name = input(dict_perguntas["nome"])
+    age = input (dict_perguntas["idade"])
+    sex = input(dict_perguntas["sexo"])
+    city = input(dict_perguntas["cidade"])
+    lista_respostas.append( {"nome":  name, 
+   "idade": age,
+   "sexo": sex,
+   "cidade": city})
+   break
 
-for pergunta in lista_perguntas:
-    resposta = input(pergunta)
-    lista_respostas.append(resposta)
+# for pergunta in lista_perguntas:
+#     resposta = input(pergunta)
+#     lista_respostas.append(resposta)
 
 
-def do_over_q():
-    do_over = int (input("Qual pergunta gostaria de refazer? Por favor selecione um número correspondente à pergunta a ser refeita"))
-    i = do_over
-    for i in range(0,4):
-        if do_over == i+1:
-            lista_respostas[i] = input (lista_perguntas[i]) 
-            break
+# def do_over_q():
+#     do_over = int (input("Qual pergunta gostaria de refazer? Por favor selecione um número correspondente à pergunta a ser refeita"))
+#     i = do_over
+#     for i in range(0,4):
+#         if do_over == i+1:
+#             lista_respostas[i] = input (lista_perguntas[i]) 
+#             break
             
 def correcao():
     refazer = int(input("Qual pergunta gostaria de refazer? Por favor selecione um número correspondente à pergunta a ser refeita"))
@@ -65,7 +77,20 @@ with open('mycsvfile.csv', 'a+') as f:
     # f.write(arquivo)
     # f.writelines(str(d.dict_values))
         
-
+def mergeDict(dict1, dict2):
+   ''' Merge dictionaries and keep values of common keys in list'''
+   dict3 = {**dict1, **dict2}
+   for key, value in dict3.items():
+       if key in dict1 and key in dict2:
+               dict3[key] = [value , dict1[key]]
+ 
+   return dict3
+ 
+# Merge dictionaries and add values of common keys in a list
+dict3 = mergeDict(dict1, dict2)
+ 
+print('Dictionary 3 :')
+print(dict3)
 
 ''' 
 Transformar tudo para dicionário/Como salvar as respostas de forma indexada? - Done
--- Criar_Excel.py
+++ Criar_Excel.py
@@ -1,11 +1,49 @@
-
-from panda import xlswriteimport, read_csv, ExcelWriter
-import numpy as np
-
-workbook = xlsxwriter.Workbook('cadastro.xlsx')
+import xlsxwriter
+workbook = xlsxwriter.Workbook('dictExcel.xlsx')
 worksheet = workbook.add_worksheet()
+dict_perguntas = {
+  "nome":  "Qual é o seu nome?", 
+   "idade": "Qual é a sua idade?",
+   "sexo": "Qual é o seu sexo?",
+   "cidade": "Onde você mora?"
+}
 
-df_new = read_csv('fichas.csv')
-writer = ExcelWriter('cadastro.xlsx', engine='xlsxwriter')
-df.to_excel(writer, sheet_name='Sheet1',startrow = 1, header = False)
-writer.save()
+dict_respostas =  {}
+  
+name = input(dict_perguntas["nome"])
+age = input (dict_perguntas["idade"])
+sex = input(dict_perguntas["sexo"])
+city = input(dict_perguntas["cidade"])
+dict_respostas["nome"] = name
+dict_respostas["idade"] = age
+dict_respostas["sexo"] = sex
+dict_respostas["cidade"] = city
+
+def mergeDict(dict_perguntas, dict_respostas):
+   dict_combined = {**dict_perguntas, **dict_respostas}
+   for key, value in dict_combined.items():
+       if key in dict_perguntas and key in dict_respostas:
+               dict_combined[key] = [value , dict_perguntas[key]]
+ 
+   return dict_combined
+ 
+dict_combined = mergeDict(dict_perguntas, dict_respostas)
+row = 0
+col = 0
+
+for key in dict_respostas.keys():
+    row = 0
+    worksheet.write(row, col, key)
+    row += 1
+    for item in dict_respostas[key]:
+        worksheet.write(row, col, item)
+        row += 1
+    col += 1
+
+workbook.close()
+
+'''
+https://thispointer.com/how-to-merge-two-or-more-dictionaries-in-python/
+https://stackoverflow.com/questions/33575376/using-user-input-to-create-dictionaries-in-python
+https://stackoverflow.com/questions/45201288/how-to-create-header-in-excel-from-a-python-dictionary-keys
+'''
